31f64edc7817a9971be811ed7d4784a870c15f1d
adi960280-cmd <adityamalhotra960@gmail.com>
2025-11-11 09:19:47 +0530

    Refactor config.py to remove sensitive data
    Removed sensitive information from config.py and retained the structure.

--- modules/config.py
+++ modules/config.py
@@ -2,13 +2,3 @@
 import os
 
 # Bot Configuration
-BOT_NAME = os.environ.get('BOT_NAME', 'ZENITH_BOT')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '7662019980:AAGQnhhOhWxi69j3aWTFfOCUNQhbgtxSz9M')
-API_ID = int(os.environ.get('API_ID', '29115102'))
-API_HASH = os.environ.get('API_HASH', '1a331db2b00e9d2decaa9c7276449eb6')
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://herukobanna:ankit999@cluster0.xs772me.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-
-# Additional Configuration
-OWNER_ID = int(os.environ.get('OWNER_ID', '1224092270'))
-LOG_CHANNEL_ID = int(os.environ.get('LOG_CHANNEL_ID', '-1002067304488'))
-
